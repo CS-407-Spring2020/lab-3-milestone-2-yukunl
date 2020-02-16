a6a75630bf50fb82a19c34cfb86435d49182e498
akun <aliceliyukun@outlook.com>
2020-02-16 17:02:15 -0600

    - after first; second segment

--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.lab3milestone2;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button firstFragment, secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // get the reference of Button's
+        firstFragment = (Button) findViewById(R.id.firstFragment);
+        secondFragment = (Button) findViewById(R.id.secondFragment);
+
+
+        // perform setOnClickListener event on First Button
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // load First fragment
+                loadFragment(new FirstFragment());
+            }
+        });
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
     }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+    }
+
 }
